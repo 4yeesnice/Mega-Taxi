8ca4a63133cb4c2d6889876e89ab433fbe651745
Erbol <esembayev.e@gmail.com>
2024-08-17 18:07:58 +0600

    4567

--- spring_intro/src/main/java/org/example/App.java
+++ spring_intro/src/main/java/org/example/App.java
@@ -1,13 +1,24 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+
+
+        Parrot p = context.getBean(Parrot.class);
+
+        whatever w = context.getBean(whatever.class);
+
+        System.out.println("Parrot's name: " + p.getName());
+
+
+
+
     }
 }
